add7eedc92f37ee014d12107037466d380ff7a95
nurcanNT <nrcntpkra@gmail.com>
2023-03-08 12:22:26 +0300

    responsive update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,10 +10,10 @@ function Location (){
                 <h3 className="headingh3">Talk Shoes</h3>
                 <p className="text2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas doloribus vitae, id dolor exercitationem unde non, quae quidem facilis dolores mollitia accusantium aliquid voluptatibus magni ducimus qui.</p>
                 <div className="share">
-                    <a href="#" className="btn "> <FaFacebook/></a>
-                    <a href="#" className="btn "> <FaTwitter/></a>
-                    <a href="#" className="btn "> <FaInstagram/></a>
-                    <a href="#" className="btn"> <FaLinkedinIn/></a>
+                    <a href="#" className="btnfooter "> <FaFacebook/></a>
+                    <a href="#" className="btnfooter "> <FaTwitter/></a>
+                    <a href="#" className="btnfooter "> <FaInstagram/></a>
+                    <a href="#" className="btnfooter"> <FaLinkedinIn/></a>
                 </div>
             </div>
         
