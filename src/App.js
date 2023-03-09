7c6daaaf573860a76e9d7f31a4af8fb0be464c78
nurcanNT <nrcntpkra@gmail.com>
2023-03-09 13:39:05 +0300

    category update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-// import { BrowserRouter, Routes,  Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
-import Product from "./components/Product/Product";
+import Skechers from "./components/Product/Skechers";
 import Adidas from "./components/Product/Adidas";
 import Banner from "./components/Banner/Banner";
 import Banner1 from "./components/Banner1/Banner1";
@@ -17,22 +17,22 @@ function App() {
     <div className="App">
      <Navbar/>
      <HomeTitle/>
-     {/* <BrowserRouter>
-      <Routes>
-        <Route path="Heeled" element={<Heeled />}>
-          <Route path="Adidas" element={<Adidas />} />
-          <Route path="Nike" element={<Nike />} />
-          <Route path="Product" element={<Product/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter> */}
-     <Heeled/>
-     <Nike/>
+     <Router>
+      <div>
+      <h1 className="heading">All Categories</h1>
+      <div className="category-menu">
+        <Link to="/adidas"><div><a href="#" className="btn">Adidas</a></div></Link>  <Link to="/nike"><div><a href="#" className="btn">Nike</a></div></Link>
+        <Link to="/heeled"><div><a href="#" className="btn">Heeled</a></div></Link> <Link to="/skechers"><div><a href="#" className="btn">Skechers</a></div></Link>
+      </div>
+      </div>
+      <Switch>
+        <Route path="/heeled">{<Heeled/>}</Route>
+        <Route path="/nike">{<Nike/>}</Route>
+        <Route path="/adidas">{<Adidas/>}</Route>
+        <Route path="/skechers">{<Skechers/>}</Route>
+      </Switch>
+    </Router>
      <Banner1/>
-     <Adidas/>
-     <Banner/>
-     <Product/>
-     <Banner2/>
      <Footer/>
     </div>
   );
